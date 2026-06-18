a390e864df46af45946119d14d9e3172b65f54e6
hmhamster <hmhamster88@gmail.com>
2026-06-18 15:43:35 +0300

    Sttistics progressbar

--- games/catan/catan-front/src/components/StatisticsComponent.vue
+++ games/catan/catan-front/src/components/StatisticsComponent.vue
@@ -2,19 +2,29 @@
     <div class="flex flex-col">
         <span>{{ t('turnCount') }} : {{ catanStatistics.turnCount }}</span>
         <h5>{{ t('dices') }}</h5>
-        <span v-for="diceVal in diceRollsPercentage">{{ diceVal }}</span>
+        <div v-for="diceVal in diceRollsStats" class="flex flex-row items-center">
+            <span style="width: 3rem;">{{ diceVal.diceValue }} : {{ diceVal.count }}</span>
+            <div class="progress" :data-label="diceVal.prcentage.toFixed(2) + '%'">
+                <span class="value" :style="`width:${diceVal.prcentage}%;`"></span>
+            </div>
+        </div>
         <h5>{{ t('resources') }}</h5>
-        <span v-for="resourceVal in resourcesPercentage">
-            {{ resourceVal }}
-        </span>
+        <div v-for="resourceVal in resourcesStats" class="flex flex-row items-center">
+            <img class="resource-icon" :src="resourcesImages[resourceVal.type]"></img>
+            <span style="width: 3rem;"> : {{ resourceVal.count }}</span>
+            <div class="progress" :data-label="resourceVal.prcentage.toFixed(2) + '%'">
+                <span class="value" :style="`width:${resourceVal.prcentage}%;`"></span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script setup lang="ts">
 import type { GameStatistics } from 'boardgame-web-common';
-import { getAllResourcesCount, type CatanGameStatistics } from 'catan-back';
+import { CatanResourceType, getAllResourcesCount, type CatanGameStatistics } from 'catan-back';
 import { computed, type PropType } from 'vue';
 import { useI18n } from 'vue-i18n';
+import { resourcesImages } from './graphics';
 
 let localization: any = {
     en: {
@@ -47,23 +57,43 @@ const { t } = useI18n({
     messages: localization
 })
 
-const diceRollsPercentage = computed(() => {
-    const result: string[] = []
+interface DiceStat {
+    diceValue: number
+    count: number
+    prcentage: number
+}
+
+const diceRollsStats = computed(() => {
+    const result: DiceStat[] = []
     const allCount = catanStatistics.value.diceNumbersRolled.filter(n => n != null).reduce((a, b) => a + b, 0)
     for (let i = 0; i < catanStatistics.value.diceNumbersRolled.length; i++) {
         const numberRolled = catanStatistics.value.diceNumbersRolled[i]
         if (numberRolled != null) {
-            result.push(`${i}: ${numberRolled} (${(numberRolled / allCount * 100).toFixed(1)}%)`)
+            result.push({
+                diceValue: i,
+                count: numberRolled,
+                prcentage: (numberRolled / allCount * 100)
+            })
         }
     }
     return result
 })
 
-const resourcesPercentage = computed(() => {
-    const result: string[] = []
+interface ResourceStat {
+    type: CatanResourceType
+    count: number
+    prcentage: number
+}
+
+const resourcesStats = computed(() => {
+    const result: ResourceStat[] = []
     const allCount = getAllResourcesCount(catanStatistics.value.resourcesReceived)
     Object.entries(catanStatistics.value.resourcesReceived).forEach(([resourcesType, resourceCount]) => {
-        result.push(`${t('resourceType.' + resourcesType)}: ${resourceCount} (${(resourceCount / allCount * 100).toFixed(1)}%)`)
+        result.push({
+            type: resourcesType as CatanResourceType,
+            count: resourceCount,
+            prcentage: (resourceCount / allCount * 100)
+        })
     })
     return result
 })
@@ -79,3 +109,37 @@ const props = defineProps({
     }
 })
 </script>
+
+<style>
+.progress {
+    margin: 0.2rem;
+    height: 1.5em;
+    width: 100%;
+    background-color: var(--oruga-control-background-color);
+    position: relative;
+    border-width: var(--oruga-control-border-width);
+    border-style: solid;
+    border-color: var(--oruga-control-border-color);
+    border-radius: var(--oruga-border-radius);
+}
+
+.progress:before {
+    content: attr(data-label);
+    font-size: 0.8em;
+    position: absolute;
+    text-align: center;
+    left: 0;
+    right: 0;
+}
+
+.progress .value {
+    background-color: var(--main-color);
+    display: inline-block;
+    height: 100%;
+}
+
+.resource-icon {
+    width: 2rem;
+    max-width: 2rem;
+}
+</style>
